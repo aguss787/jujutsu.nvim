469fc5d634456de7bfbb08ab47f6636b8cf1b568
Agus Sentosa Hermawan <aguss787@proton.me>
2026-02-18 00:27:49 +0800

    feat: open description editor on 'd' in log buffer

--- lua/jujutsu/init.lua
+++ lua/jujutsu/init.lua
@@ -80,6 +80,42 @@ M.log = function()
     end
   end, { buffer = log_buf, desc = "jj edit revision under cursor" })
 
+  vim.keymap.set("n", "d", function()
+    local line = vim.api.nvim_get_current_line()
+    local rev = line:match("[@◉○]%s+(%w+)")
+    if not rev then
+      return
+    end
+
+    local result = vim.system({ "jj", "log", "-r", rev, "-T", "description", "--no-graph" }, { text = true }):wait()
+    local current_desc = (result.code == 0 and result.stdout or ""):gsub("\n$", "")
+
+    local desc_buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_name(desc_buf, "jj://describe/" .. rev)
+    vim.api.nvim_buf_set_lines(desc_buf, 0, -1, false, vim.split(current_desc, "\n", { plain = true }))
+    vim.bo[desc_buf].buftype = "acwrite"
+    vim.bo[desc_buf].bufhidden = "wipe"
+    vim.bo[desc_buf].filetype = "gitcommit"
+
+    vim.api.nvim_create_autocmd("BufWriteCmd", {
+      buffer = desc_buf,
+      callback = function()
+        local desc = table.concat(vim.api.nvim_buf_get_lines(desc_buf, 0, -1, false), "\n")
+        local describe_result = vim.system({ "jj", "describe", "-r", rev, "-m", desc }, { text = true }):wait()
+        if describe_result.code ~= 0 then
+          vim.notify("jj describe: " .. (describe_result.stderr or "unknown error"), vim.log.levels.ERROR)
+        else
+          vim.bo[desc_buf].modified = false
+          refresh_log_buf(log_buf)
+        end
+      end,
+    })
+
+    vim.cmd.split()
+    vim.api.nvim_win_set_buf(0, desc_buf)
+    vim.cmd.startinsert()
+  end, { buffer = log_buf, desc = "Set revision description" })
+
   vim.cmd.split()
   vim.api.nvim_win_set_buf(0, log_buf)
 end
